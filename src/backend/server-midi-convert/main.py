6f9c72c09e1ae9d15ded11b91ff3bcca8fd34b27
BenedictPresley <19623252@std.stei.itb.ac.id>
2024-12-15 22:08:33 +0700

    feat: midi to array

--- src/backend/server-midi-convert/main.py
+++ src/backend/server-midi-convert/main.py
@@ -3,6 +3,9 @@ from basic_pitch import ICASSP_2022_MODEL_PATH
 import os
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from mido import MidiFile
+import json
+import uuid
 import logging
 
 
@@ -34,10 +37,13 @@ async def convert_to_midi(request: FilePathRequest):
         # Generate MIDI file
         midi_file_path = convert_audio_to_midi(file_path)
         full_midi_path = os.path.abspath(midi_file_path)
+        midi_data_array = convert_midi_to_array(midi_file_path)
+        json_file_path = save_midi_array_to_json(midi_data_array, midi_file_path, os.path.basename(file_path))
 
+        
         # Return the full path of the generated MIDI file
         logger.info(f"MIDI file created at: {full_midi_path}")
-        return {"full_path": full_midi_path}
+        return {"full_path": full_midi_path, "json_file_path": json_file_path,}
     except Exception as e:
         logger.error(f"Error processing file: {str(e)}")
         raise HTTPException(
@@ -67,3 +73,34 @@ def convert_audio_to_midi(file_path: str) -> str:
     # Create the MIDI output file path by appending '_basic_pitch.mid'
     output_filename = f"{os.path.splitext(os.path.basename(file_path))[0]}_basic_pitch.mid"
     return os.path.join(output_dir, output_filename)
+
+# Helper Function: Convert MIDI to Array
+def convert_midi_to_array(midi_file_path: str):
+    """
+    Parse a MIDI file and convert its content to an array of integers.
+    Each integer represents a MIDI note that is on.
+    """
+    midi_data_array = []
+    midi = MidiFile(midi_file_path)
+
+    for track in midi.tracks:
+        for msg in track:
+            if msg.type == "note_on" and msg.velocity > 0:
+                midi_data_array.append(msg.note)
+
+    return midi_data_array
+
+# Helper Function: Save MIDI Array to JSON
+def save_midi_array_to_json(midi_data_array, midi_file_path, original_filename):
+    """
+    Save the MIDI data array as a JSON file. Use the original file name with a unique identifier for the JSON file.
+    """
+    output_dir = os.path.dirname(midi_file_path)
+    unique_id = uuid.uuid4().hex  # Generate a unique identifier
+    json_filename = f"{os.path.splitext(original_filename)[0]}_{unique_id}_data.json"
+    json_file_path = os.path.join(output_dir, json_filename)
+
+    with open(json_file_path, "w") as json_file:
+        json.dump(midi_data_array, json_file, indent=4)
+
+    return json_file_path
